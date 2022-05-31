bad99bae385cbb980e1c07677aee8e7412f8d963
Erodri6z <erikrodriguez2417@gmail.com>
2022-05-31 15:58:03 -0500

    initial comit and complete setup

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let taskList
+
+
+
+let tsk = document.getElementById("#task")
+let btn = document.getElementById("#submit-button")
+let lst = document.getElementById("#todo-list")
+ 
+function (){
+    taskList = tsk.value
+}
+
+console.log(tsk.value)
+// function makelist(){
+//     tsk.value.push(document.createElement(li))
+// }
